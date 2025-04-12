b30db2bc7124f16972112498e9d3fcd09e9f12d7
Maëlle Nioche <maelle.nioche@gmail.com>
2025-04-12 23:58:34 +0200

    feat: add burger menu functionality and overlay for mobile navigation

--- src/JS/script.js
+++ src/JS/script.js
@@ -25,3 +25,34 @@ if (storedTheme) {
 toggle.addEventListener('change', () => {
   applyTheme(toggle.checked ? 'dark' : 'light');
 });
+
+// Menu burger
+
+const overlay = document.querySelector('.header-overlay');
+const burger = document.getElementById('burger');
+const headerLinks = document.querySelectorAll('header nav a');
+
+burger.addEventListener('click', (e) => {
+  const body = document.body;
+  if (e.target.checked) {
+    body.classList.add('no-scroll');
+  } else {
+    body.classList.remove('no-scroll');
+  }
+});
+
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
+    overlay.classList.remove('active');
+    burger.checked = false;
+  }
+});
+
+headerLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (burger.checked) {
+      burger.checked = false;
+      document.body.classList.remove('no-scroll');
+    }
+  });
+});
