8fe74b286adb9bd8ae39f96513015f1b36db00ac
Maëlle Nioche <maelle.nioche@gmail.com>
2025-04-15 17:56:59 +0200

    feat: add 404 page; correct links in about page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         about: path.resolve(__dirname, 'about.html'),
+        notfound: path.resolve(__dirname, '404.html'),
       },
     },
   },
